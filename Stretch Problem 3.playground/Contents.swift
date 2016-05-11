52ed7c418205012de6185f19a7f63a848d0c0fc3
Eva <Bresciano>
2016-05-11 10:09:03 -0600

    finished stretch 3

--- Stretch Problem 3.playground/Contents.swift
+++ Stretch Problem 3.playground/Contents.swift
@@ -4,7 +4,7 @@ import Cocoa
 
 var str = "Hello, playground"
 
-class Person {
+class Person: Equatable {
 
     let firstName: String
     let lastName: String
@@ -17,21 +17,25 @@ class Person {
     }
 }
 
+func ==(lhs: Person, rhs: Person) -> Bool {
+    return lhs.age == rhs.age && lhs.firstName == rhs.firstName && lhs.lastName == rhs.lastName
+}
+
 let james = Person(firstName: "James", lastName: "Pacheo", age: 26)
 let andrea = Person(firstName: "Andrea", lastName: "Mower", age: 24)
 let carol = Person(firstName: "Rebecca", lastName: "Mordo", age: 30)
+var clubMembers: [Person] = [james, andrea, carol,]
 let freddie = Person(firstName: "Freddie", lastName: "Cocherella", age: 50)
 let pete = Person(firstName: "Pete", lastName: "Thefish", age: 49)
-let clubMembers:[Person] = [james, andrea, carol, pete]
 
-func areYouAMemberOfTheClub(Person) -> Bool {
-    if Person == clubMembers {
-        print(true) }
-    
-    else {
-        print(false)
-    
+clubMembers.append(pete)
+
+    func areYouAMemberOfTheClub(person: Person) -> Bool {
+    return clubMembers.contains(person)
+   
     
 }
 
-}
+
+areYouAMemberOfTheClub(pete)
+areYouAMemberOfTheClub(freddie)
